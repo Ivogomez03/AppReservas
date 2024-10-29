63fc1268e8a82756e9f0dcd95a280ccff7bb745d
Tomas Sanchez <tomasgsanchezz@gmail.com>
2024-10-29 18:33:53 -0300

    Administrador

--- backend/src/main/java/com/example/backend/Logica/CU13/Administrador.java
+++ backend/src/main/java/com/example/backend/Logica/CU13/Administrador.java
@@ -1,3 +1,12 @@
 package com.example.backend.Logica.CU13;
 
 import com.example.backend.Logica.usuario;
+public class Administrador extends usuario{
+
+public Administrador(String nombre, String apellido, int id_usuario, String contraseña) {
+        super(nombre, apellido, id_usuario, contraseña);
+    }
+//public Bedel CrearBedel() {
+// Bedel bedel = new Bedel(String nombre, String apellido, int id_usuario, String contraseña, TurnoDeTrabajo turno);
+//}
+}
