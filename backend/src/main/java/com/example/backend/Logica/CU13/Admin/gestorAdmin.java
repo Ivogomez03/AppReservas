fa5c7cf936c668926d9f40625b19f3ccef9877bd
Dracusnobilacus <mariobettati45@gmail.com>
2024-10-31 18:53:06 -0300

    contra actualizada

--- backend/src/main/java/com/example/backend/Logica/CU13/Admin/gestorAdmin.java
+++ backend/src/main/java/com/example/backend/Logica/CU13/Admin/gestorAdmin.java
@@ -1,7 +1,5 @@
  package com.example.backend.Logica.CU13.Admin;
 
-import java.sql.SQLException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- backend/src/main/java/com/example/backend/Logica/CU13/Bedel/gestorBedel.java
+++ backend/src/main/java/com/example/backend/Logica/CU13/Bedel/gestorBedel.java
@@ -14,9 +14,34 @@ public class gestorBedel {
    
     @PostMapping("/crear")
     public String crearBedel(@RequestBody BedelDTO bedelDTO) {
+
+        String contra = bedelDTO.getContrasena();
+        Boolean caracteresEspeciales = false;
+
+        String specialChars = "@#$%&*";
+        for (char c : contra.toCharArray()) {
+            if (specialChars.contains(String.valueOf(c))) {
+                caracteresEspeciales = true;
+            }
+        }
+
         if (bedelDAO.existeBedelPorID(bedelDTO.getIdUsuario())) {
             return "El ID del Bedel ya existe.";
-        } else {
+        } 
+        else if(contra.length() < 4) {
+            return "La contraseña debe tener al menos 4 caracteres.";
+        }
+        else if(contra.chars().anyMatch(Character::isDigit)){
+            return "La contraseña debe tener al menos un número.";
+        }
+        else if(contra.chars().anyMatch(Character::isUpperCase)){
+            return "La contraseña debe tener al menos una mayúscula.";
+        }
+        else if(!caracteresEspeciales){
+            return "La contraseña debe tener al menos un caracter especial.";
+        }
+
+        else {
             bedelDAO.guardarBedel(bedelDTO);
             return "Bedel creado exitosamente.";
         }
