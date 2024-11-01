603296313651a2527cf16c8d6950e717216deb90
Migue <miguechiape@gmail.com>
2024-10-31 21:13:43 -0300

    Solucion de error de migue

--- backend/src/main/java/com/example/backend/Logica/usuario.java
+++ backend/src/main/java/com/example/backend/Logica/usuario.java
@@ -14,6 +14,7 @@ public abstract class usuario {
     @Id 
     @Column(name = "idUsuario")
     private int idUsuario;
+     @Column(name = "contrasena")
     private String contrasena;
     
     public usuario() {
